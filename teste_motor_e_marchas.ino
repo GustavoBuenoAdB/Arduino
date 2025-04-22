8a1bf872ac114000586c8c3a762dca1719334cdd
Gustavo Bueno <gustavobuenodacosta@gmail.com>
2025-04-22 13:13:18 -0300

    Update teste_motor_e_marchas.ino

--- teste_motor_e_marchas.ino
+++ teste_motor_e_marchas.ino
@@ -1,44 +1,47 @@
 #include <AFMotor.h>
 
-// numero de marchas definidas
+// definicoes para as marchas
 #define N_MARCHAS 5
+#define MIN_VEL 155
+
+// definicoes para o sensor
+#define DISTANCIA_MIM 25
+
+//definicoes para curvas
+#define TEMPO_CURVA_ANG_M1 2
+#define ESQUERDA 0
+#define DIREITA 1
 
 //definindo as portas usadas
 #define PORTA_MOT_E 1
 #define PORTA_MOT_D 2
 
-//definindo esquerda e direita pra curvas
-#define DIREITA 1
-#define ESQUERDA 0
-
 //definindo motores como globais
-AF_DCMotor* motor_esq;
-AF_DCMotor* motor_dir;
+AF_DCMotor motor_esq(PORTA_MOT_E);
+AF_DCMotor motor_dir(PORTA_MOT_D);
 
 uint8_t incremento_marcha; 
 
 void setup() 
 {
-  motor_esq = new AF_DCMotor(PORTA_MOT_E);
-  motor_dir = new AF_DCMotor(PORTA_MOT_D);
-
-  incremento_marcha = (uint8_t) 255 / N_MARCHAS;
+  incremento_marcha = (uint8_t) (255 - MIN_VEL) / N_MARCHAS;
 }
 
 void loop() 
 {
-  uint8_t velocidade = 0;
-  uint8_t marcha = 0;
+  uint8_t velocidade = MIN_VEL;
+  uint8_t marcha = 1;
 
+  set_motores(velocidade, FORWARD);
+  
   delay (10000);
 
-  troca_marcha(&marcha, 2, &velocidade);
+  //testando qual o angulo de uma rotacao em M1
+  curva(esquerda, 360);
 
-  delay (5000);
+  //troca_marcha(&marcha, 1, &velocidade);
 
-  troca_marcha(&marcha, 0, &velocidade);
-
-  curva(DIREITA, &marcha, &velocidade);
+  
 }
 
 
@@ -74,9 +77,9 @@ void troca_marcha(uint8_t* atual, uint8_t destino, uint8_t* velocidade)
     troca_marcha(atual, destino, velocidade);
   }
   
-  else if (*atual > destino)
+  else if (*atual > destino && destino >= 1)
   {
-    //acelera aos pucos até o valor da proxima marcha
+    //desacelera aos pucos até o valor da marcha anterior
     uint8_t i;
     for (i = *velocidade ; i > *velocidade - incremento_marcha ; i--)
     {
@@ -91,7 +94,6 @@ void troca_marcha(uint8_t* atual, uint8_t destino, uint8_t* velocidade)
     delay(1000);
     troca_marcha(atual, destino, velocidade);
   }
-   
 }
 
 /**
@@ -105,33 +107,41 @@ void set_motores(uint8_t velocidade, int direcao)
 {
   if (velocidade == 0)
   {
-    motor_esq -> setSpeed(velocidade);
-    motor_dir -> setSpeed(velocidade);
-    motor_esq -> run(RELEASE);
-    motor_dir -> run(RELEASE);
+    motor_esq.setSpeed(velocidade);
+    motor_dir.setSpeed(velocidade);
+    motor_esq.run(RELEASE);
+    motor_dir.run(RELEASE);
   }
   else
   {
-    motor_esq -> setSpeed(velocidade);
-    motor_dir -> setSpeed(velocidade);
-    motor_esq -> run(direcao);
-    motor_dir -> run(direcao);
+    motor_esq.setSpeed(velocidade);
+    motor_dir.setSpeed(velocidade);
+    motor_esq.run(direcao);
+    motor_dir.run(direcao);
   }
 }
 
-void curva(uint8_t direcao, uint8_t* marcha, uint8_t* velocidade)
+
+/**
+ * @brief faz uma curva em um angulo qualquer
+ *
+ * faz uma curva em um angulo e direcao passados por parametros
+ * @warning o calculo do angulo supoe que ele esta em uma marcha 1. 
+ *
+ * @param direcao a direcao da curva (DIREITA ou ESQUERDA).
+ * @param angulo o angulo de rotacao da curva. (depende da macro TEMPO_CURVA_ANG_M1)
+*/
+void curva(int direcao, uint8_t angulo)
 {
-    troca_marcha(marcha, 1, velocidade);
-    if (direcao == ESQUERDA)
-    {
-        motor_esq -> run(BACKWARD);
-        motor_dir -> run(FORWARD);
-    }
-    else if (direcao == DIREITA)
-    {
-        motor_dir -> run(BACKWARD);
-        motor_esq -> run(FORWARD);
-    }
-    delay(200);
-   troca_marcha (marcha, 0, velocidade);  
+   if (direcao == ESQUERDA)
+   {
+     motor_esq.run(FORWARD);
+     motor_dir.run(BACKWARD);
+   }
+   else if (direcao == DIREITA)
+   {
+     motor_esq.run(BACKWARD);
+     motor_dir.run(FORWARD);
+   }
+   delay(angulo * TEMPO_CURVA_ANG_M1);
 }
