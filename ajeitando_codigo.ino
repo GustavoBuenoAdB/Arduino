ef1121a671f86ba75382c2fd50d0878e92d5ec94
Gustavo Bueno <gustavobuenodacosta@gmail.com>
2025-04-25 13:13:55 -0300

    Add files via upload
    comentando o codigo e iniciando os tratamentos com encoders, ajeitando as funcoes curva e andar para funcionar baseadas nos encoders

--- ajeitando_codigo.ino
+++ ajeitando_codigo.ino
@@ -24,7 +24,61 @@
 #define PORTA_MOT_D 1
 //==================================
 
+//=== Definições dos encoders ======
+#define PINO_ENC_ESQ 18 // interrupcao 5
+#define PINO_ENC_DIR 19 // interrupcao 4
+#define N_ENC_POR_VOLTA 20 //20 pra falling
+#define DIST_POR_ENC 1.078
+#define ANG_POR_ENC 9.5071
+#define VAL_ERRO_ENC_1 0.0894
+//==================================
+
+//=== Definições do comportamento ==
+#define SEGURANCA 1 // faz curva só na marcha 1
+//==================================
+
+
 // === Definindo as funções usadas =========================================
+
+/**
+ * @brief Inicia os 2 motores em marcha 1.
+ *
+ * inicia os valores base de velocidade e de marcha da maneira correta.
+*/
+void da_partida();
+
+/**
+ * @brief Desliga os 2 motores.
+ *
+ * desliga e reinicia os valores bases da maneira correta para a proxima partida.
+*/
+void desliga();
+
+/**
+ * @brief Anda pra frente ou para traz uma distancia X
+ *
+ * Anda pra frente ou para traz uma distancia de X cm passada por parametro
+ * a distancia realmente percorrida é aproximada para valores multiplos de DIST_POR_ENC
+ * 
+ * @note a distancia independe da marcha atual. 
+ *
+ * @param direcao A direcao para qual andar (FORWARD ou BACKWARD).
+ * @param centimetros A distancia a ser percorrida em cm. (essa distancia sera arredondado para um multiplo de DIST_POR_ENC).
+*/
+void anda(int direcao, double centimetros);
+
+/**
+ * @brief Faz uma curva em um angulo escolhido
+ *
+ * faz uma curva em um angulo (nao negativo) e direcao passados por parametros
+ * o angulo realmente virado é aproximado para valores multiplos de ANG_POR_ENC
+ * 
+ * @note com SEGURANCA = 1 a funcao troca pra marcha 1 antes de virar. 
+ *
+ * @param direcao a direcao da curva (DIREITA ou ESQUERDA).
+ * @param angulo o angulo de rotacao da curva. (esse angulo sera arredondado para um multiplo de ANG_POR_ENC)
+*/
+void curva(int direcao, double angulo);
 
 /**
  * @brief Incrementa ou decrementa a marcha atual.
@@ -35,7 +89,7 @@
  *
  * @param destino Valor da marcha que se quer chegar (entre [0 - N_MARCHAS])
 */
-void troca_marcha(uint8_t destino);
+void set_marcha(uint8_t destino);
 
 /**
  * @brief Atualiza a velocidade e a direcao de ambos os motores
@@ -43,44 +97,66 @@ void troca_marcha(uint8_t destino);
  * @warning set_motores é uma função usada para implementar outras funções, usa-la diretamente no codigo de loop() pode gerar problemas.
  *  
  * @param velocidade a nova velocidade dos motores
- * @param direcao a direcao dos motores (FORWARD, BACKWARD ou RELEASE) (ignorada se velocidade = 0)
- *
+ * @param direcao_esq a direcao do motor esquerdo (FORWARD, BACKWARD ou RELEASE)
+ * @param direcao_dir a direcao do motor direito (FORWARD, BACKWARD ou RELEASE)
 */
-void set_motores(uint8_t velocidade, int direcao);
+void set_motores(uint8_t velocidade, int direcao_esq, int direcao_dir);
 
 /**
- * @brief Faz uma curva em um angulo qualquer
+ * @brief Atualiza a os contadores dos encoders de cada motor
  *
- * faz uma curva em um angulo e direcao passados por parametros
- * como o depende da potencia no motor, o calculo é aproximado e
- * supoe que ele esta em uma marcha 1. 
- *
- * @param direcao a direcao da curva (DIREITA ou ESQUERDA).
- * @param angulo o angulo de rotacao da curva. (depende da macro TEMPO_CURVA_ANG_M1)
+ * @warning set_contadores é uma função usada para implementar outras funções, usa-la diretamente no codigo de loop() pode gerar problemas.
+ *  
+ * @param esq O valor do contador do motor esquerdo.
+ * @param dir O valor do contador do motor direito.
 */
-void curva(int direcao, int angulo);
+void set_contadores(int esq, int dir);
+
+/**
+ * @brief Função chamada pela interrupção do encoder esquerdo.
+ *
+ * atualiza o contador do encoder esquerdo e tenta tratar o
+ * erro de sincronia caso ele seja detectado.
+ * 
+ * @warning interrupcao_esq é uma função usada para implementar outras funções, usa-la diretamente no codigo de loop() pode gerar problemas.
+*/
+void interrupcao_esq();
+
+/**
+ * @brief Função chamada pela interrupção do encoder direito.
+ *
+ * atualiza o contador do encoder direito e tenta tratar o 
+ * erro de sincronia caso ele seja detectado.
+ * 
+ * @warning interrupcao_dir é uma função usada para implementar outras funções, usa-la diretamente no codigo de loop() pode gerar problemas.
+*/
+void interrupcao_dir();
 
 /**
  * @brief Retorna a distancia lida pelo sensor.
  *
  * Com o sensor de profundidade calcula e ja divide pelo PARAM_DIV_SENSOR
  * retornando a distancia aproximada em centimetros do objeto mais proximo
- * na linha do sensor 
+ * na linha do sensor e atualizando a variavel global distancia.
  *
  * @return um valor entre [5 - 250] estimativamente ou um valor negativo, caso esteja fora do alcançe.
 */
 int confere_distancia();
-void da_partida();
-void desliga();
 // =========================================================================
 
 //=== Definindo variaveis de escopo global =================================
-AF_DCMotor motor_esq(PORTA_MOT_E);
-AF_DCMotor motor_dir(PORTA_MOT_D);
+AF_DCMotor Motor_esq(PORTA_MOT_E);
+AF_DCMotor Motor_dir(PORTA_MOT_D);
+//valor alterado a cada troca de marcha
 uint8_t incremento_marcha = (uint8_t) (255 - MIN_VEL) / N_MARCHAS;
+// distancia captada pelo sensor
 int distancia;
 uint8_t velocidade = 0;
 uint8_t marcha = 0;
+//contador de sinais que cada encoder de motor deve dar antes de parar
+volatile int cont_dir = 0;
+volatile int cont_esq = 0;
+volatile double erro_acumulado = 0;
 //==========================================================================
 
 void setup() 
@@ -88,6 +164,8 @@ void setup()
   //definindo as portas de entrada e saida.
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
+  pinMode(PINO_ENC_ESQ, INPUT);
+  pinMode(PINO_ENC_DIR, INPUT);
 }
 
 void loop() 
@@ -120,14 +198,15 @@ int confere_distancia()
   digitalWrite(TRIG, HIGH);
   delay(1);
   digitalWrite(TRIG, LOW);
-  return ( pulseIn(ECHO, HIGH) / PARAM_DIV_SENSOR ); 
+  distancia = ( pulseIn(ECHO, HIGH) / PARAM_DIV_SENSOR );
+  return distancia; 
 }
 
 void da_partida()
 {
   atual = 1;
   velocidade = MIN_VEL;
-  set_motores(velocidade, FORWARD);
+  set_motores(velocidade, FORWARD, FORWARD);
 }
 
 void desliga()
@@ -175,27 +254,65 @@ void troca_marcha(uint8_t destino)
   }
 }
 
-void set_motores(uint8_t velocidade, int direcao)
+void set_motores(uint8_t velocidade, int direcao_esq, int direcao_dir)
 {
-  motor_esq.setSpeed(velocidade);
-  motor_dir.setSpeed(velocidade);
-  motor_esq.run(direcao);
-  motor_dir.run(direcao);
+  Motor_esq.setSpeed(velocidade);
+  Motor_dir.setSpeed(velocidade);
+  Motor_esq.run(direcao_esq);
+  Motor_dir.run(direcao_dir);
 }
 
-void curva(int direcao, int angulo, uint8_t* atual, uint8_t* velocidade)
+void curva(int direcao, double angulo)
 {
-   troca_marcha(1);
+  if (SEGURANCA)
+    troca_marcha(1);
    
-   if (direcao == ESQUERDA)
-   {
-     motor_esq.run(FORWARD);
-     motor_dir.run(BACKWARD);
-   }
-   else if (direcao == DIREITA)
-   {
-     motor_esq.run(BACKWARD);
-     motor_dir.run(FORWARD);
-   }
-   delay(angulo * TEMPO_CURVA_ANG_M1);
+  if (direcao == ESQUERDA)
+    set_motores(velocidade, FORWARD, BACKWARD);
+  else if (direcao == DIREITA)
+    set_motores(velocidade, BACKWARD, FORWARD);
+
+  set_contadores( (int) angulo/ANG_POR_ENC , (int) angulo/ANG_POR_ENC );
+}
+
+void anda(int direcao, double centimetros)
+{
+  set_motores(velocidade, direcao, direcao);
+  set_contadores((int) centimetros / DIST_POR_ENC , (int) centimetros / DIST_POR_ENC);
+}
+
+void set_contadores(int esq, int dir)
+{
+  cont_dir = dir;
+  cont_esq = esq;
+}
+
+void interrupcao_esq()
+{
+  if (cont_esq > 0)
+    cont_esq--;
+  else
+  {
+    Motor_esq.run(RELEASE);
+    if (cont_dir > 0)
+      // Quase impossivel rodar isso pq VAL_ERRO_ENC_1 maximo é muito baixo
+      erro_acumulado += (cont_dir * VAL_ERRO_ENC_1);
+      if (erro_acumulado > DIST_POR_ENC)
+        set_contadores(0 , (int) erro_acumulado / DIST_POR_ENC);
+  }
+}
+
+void interrupcao_dir()
+{
+  if (cont_dir > 0)
+    cont_dir--;
+  else
+  {
+    Motor_dir.run(RELEASE);
+    if (cont_esq > 0)
+      // Quase impossivel rodar isso pq VAL_ERRO_ENC_1 maximo é muito baixo
+      erro_acumulado -= (cont_esq * VAL_ERRO_ENC_1);
+      if (erro_acumulado < -DIST_POR_ENC)
+        set_contadores((int) -erro_acumulado / DIST_POR_ENC, 0);
+  }
 }
