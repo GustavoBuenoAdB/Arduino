e50ab468caab39a9441b6b3ffad77bc55ad0436e
Gustavo Bueno <gustavobuenodacosta@gmail.com>
2025-05-27 10:31:59 -0300

    Add files via upload
    Corrigindo o uso dos shield para outra versao com portas, ajustando algumas macros e o executa comando para começar a testar no robo de verdade

--- prototipo_segue_linha.ino
+++ prototipo_segue_linha.ino
@@ -9,25 +9,30 @@
 #define PSLP_DIR_MEIO 10
 
 // Porta Motor Esquerdo
-#define PORTA_MOT_E 1
-// Porta Motor Direito
-#define PORTA_MOT_D 2
+#define PB1A_MOTOR_ESQ
+#define PB1B_MOTOR_ESQ
+#define PA1A_MOTOR_DIR
+#define PA1B_MOTOR_DIR
+
+#define AVANCAR 1
+#define RETORNAR -1
+#define PARAR 0
 
 // Definicoes de Valores para traducao real ===
 
 // velocidades
-#define VEL_LENTA 1
-#define VEL_PADRAO 1
-#define VEL_RAPIDA 1
+#define VEL_LENTA 180
+#define VEL_PADRAO 220
+#define VEL_RAPIDA 255
 
 // duracoes
-#define DURACAO_CURTA 1
-#define DURACAO_PADRAO 1
-#define DURACAO_LONGA 1
+#define DURACAO_CURTA 2
+#define DURACAO_PADRAO 5
+#define DURACAO_LONGA 10
 
 // duracoes para cada angulo de curva em cada velocidade
-#define DURACAO_ANG_CURVA_P_VEL_LENTA 1
-#define DURACAO_ANG_CURVA_P_VEL_PADRAO 1
+#define DURACAO_ANG_CURVA_P_VEL_LENTA 3
+#define DURACAO_ANG_CURVA_P_VEL_PADRAO 2
 #define DURACAO_ANG_CURVA_P_VEL_RAPIDA 1
 
 
@@ -39,8 +44,8 @@ int sen_Dir_Meio = 0;
 
 typedef struct Comando
 {
-    int mov_MotEsq; // define se o motor avança (1) retrocede (-1) ou fica parado (0).
-    int mov_MotDir; // define se o motor avança (1) retrocede (-1) ou fica parado (0).
+    int sent_MotEsq; // define se o motor avança (1) retrocede (-1) ou fica parado (0).
+    int sent_MotDir; // define se o motor avança (1) retrocede (-1) ou fica parado (0).
     int mov_duracao; // duracao em ms do movimento dos motores.
     uint8_t mov_velocidade; // velocidade pra aplicar idealmente em ambos os motores.
     int leitura_anterior[4]; // leitura anterior dos 4 sensores de linha.
@@ -49,18 +54,9 @@ typedef struct Comando
 
 Comando comando;
 
-#define MOV_LEVE 1
-#define MOV_BRUCO 2
-#define MOV_DELAY 3.0
-
-AF_DCMotor Motor_esq(PORTA_MOT_E);
-AF_DCMotor Motor_dir(PORTA_MOT_D);
-
-uint8_t velocidade = 200;
-
 #define INC_MOTOR_REAL 0.02 // 2%
-double porcentagem_motor_Esq_real = 0.9;
-double porcentagem_motor_Dir_real = 0.9;
+double porcentagem_motor_Esq_real = 1;
+double porcentagem_motor_Dir_real = 1;
 
 void setup()
 {
@@ -69,6 +65,12 @@ void setup()
     pinMode(PSLP_DIR_LADO, INPUT);
     pinMode(PSLP_ESQ_MEIO, INPUT);
     pinMode(PSLP_DIR_MEIO, INPUT);
+
+    // inicializando portas dos Motores
+    pinMode(PA1A_MOTOR_DIR, OUTPUT);
+    pinMode(PA1B_MOTOR_DIR, OUTPUT);
+    pinMode(PB1A_MOTOR_ESQ, OUTPUT);
+    pinMode(PB1B_MOTOR_ESQ, OUTPUT);
 }
 
 // loop de estados simples para gerar rotina
@@ -96,8 +98,8 @@ void define_Comando()
     // if (B - P - P - B) {...}
     if ( !(sen_Esq_Lado) && (sen_Esq_Meio) && (sen_Dir_Meio) && !(sen_Dir_Lado) )
     {
-        comando.mov_MotEsq = FORWARD;
-        comando.mov_MotDir = FORWARD;
+        comando.sent_MotEsq = AVANCAR;
+        comando.sent_MotDir = AVANCAR;
         comando.mov_velocidade = VEL_PADRAO;
         comando.mov_duracao = DURACAO_PADRAO;
     }
@@ -105,8 +107,8 @@ void define_Comando()
     // if (B - B - P - B) {...}
     else if ( !(sen_Esq_Lado) && !(sen_Esq_Meio) && (sen_Dir_Meio) && !(sen_Dir_Lado) )
     {
-        comando.mov_MotEsq = RELEASE;
-        comando.mov_MotDir = BACKWARD;
+        comando.sent_MotEsq = PARAR;
+        comando.sent_MotDir = RETORNAR;
         comando.mov_velocidade = VEL_PADRAO;
         comando.mov_duracao = DURACAO_CURTA;
     }
@@ -114,8 +116,8 @@ void define_Comando()
     // if (B - P - B - B) {...}
     else if ( !(sen_Esq_Lado) && (sen_Esq_Meio) && !(sen_Dir_Meio) && !(sen_Dir_Lado) )
     {
-        comando.mov_MotEsq = BACKWARD;
-        comando.mov_MotDir = RELEASE;
+        comando.sent_MotEsq = RETORNAR;
+        comando.sent_MotDir = PARAR;
         comando.mov_velocidade = VEL_PADRAO;
         comando.mov_duracao = DURACAO_CURTA;
     }
@@ -123,8 +125,8 @@ void define_Comando()
     // if (P - P - P - P) {...}
     else if ( (sen_Esq_Lado) && (sen_Esq_Meio) && (sen_Dir_Meio) && (sen_Dir_Lado) )
     {
-        comando.mov_MotEsq = RELEASE;
-        comando.mov_MotDir = RELEASE;
+        comando.sent_MotEsq = PARAR;
+        comando.sent_MotDir = PARAR;
     }
 
     // Situacao Problema.
@@ -132,8 +134,8 @@ void define_Comando()
     else if ( !(sen_Esq_Lado) && !(sen_Esq_Meio) && !(sen_Dir_Meio) && !(sen_Dir_Lado) )
     {
         // supondo um gap
-        comando.mov_MotEsq = FORWARD;
-        comando.mov_MotDir = FORWARD;
+        comando.sent_MotEsq = AVANCAR;
+        comando.sent_MotDir = AVANCAR;
         // diminui a velocidade pra tentar evitar de ignorar uma linha em s1 e s4
         comando.mov_velocidade = VEL_LENTA;
         comando.mov_duracao = DURACAO_CURTA;
@@ -142,8 +144,8 @@ void define_Comando()
     // if (B - P - P - P) ou (B - B - B - P) {...}
     else if ( !(sen_Esq_Lado) && ( ( (sen_Esq_Meio) && (sen_Dir_Meio) ) || ( !(sen_Esq_Meio) && !(sen_Dir_Meio) ) ) && (sen_Dir_Lado) )
     {
-        comando.mov_MotEsq = FORWARD;
-        comando.mov_MotDir = BACKWARD;
+        comando.sent_MotEsq = AVANCAR;
+        comando.sent_MotDir = PARAR;
         // diminui a velocidade pra tentar evitar de ignorar uma linha em s1 e s4
         comando.mov_velocidade = VEL_LENTA;
         comando.mov_duracao = DURACAO_ANG_CURVA_P_VEL_LENTA * 90;
@@ -152,8 +154,8 @@ void define_Comando()
     // if (P - P - P - B) ou (P - B - B - B) {...}
     else if ( (sen_Esq_Lado) && ( ( (sen_Esq_Meio) && (sen_Dir_Meio) ) || ( !(sen_Esq_Meio) && !(sen_Dir_Meio) ) ) && !(sen_Dir_Lado) )
     {
-        comando.mov_MotEsq = BACKWARD;
-        comando.mov_MotDir = FORWARD;
+        comando.sent_MotEsq = RETORNAR;
+        comando.sent_MotDir = AVANCAR;
         // diminui a velocidade pra tentar evitar de ignorar uma linha em s1 e s4
         comando.mov_velocidade = VEL_LENTA;
         comando.mov_duracao = DURACAO_ANG_CURVA_P_VEL_LENTA * 90;
@@ -163,15 +165,47 @@ void define_Comando()
 
 void executa_Comando()
 {
-    set_Motores(comando.mov_velocidade, comando.mov_MotEsq, comando.mov_MotDir);
+    set_motores(comando.mov_velocidade, comando.sent_MotEsq, comando.sent_MotDir);
     delay(comando.mov_duracao);
 }
 
 
+
 void set_motores(uint8_t velocidade, int direcao_esq, int direcao_dir)
 {
-  Motor_esq.setSpeed((uint8_t) (velocidade * porcentagem_motor_Esq_real));
-  Motor_dir.setSpeed((uint8_t) (velocidade * porcentagem_motor_Dir_real));
-  Motor_esq.run(direcao_esq);
-  Motor_dir.run(direcao_dir);
+    if (direcao_esq == AVANCAR)
+    {
+        // 1 - % pq nesse sentido o calculo é invertido
+        int pwm = max(0, min(255, int(velocidade * (1.0 - porcentagem_motor_Esq_real))));
+        analogWrite(PB1A_MOTOR_ESQ, pwm); 
+        digitalWrite(PB1B_MOTOR_ESQ, LOW);
+    }
+    else if (direcao_esq == RETORNAR)
+    {
+        digitalWrite(PB1A_MOTOR_ESQ, LOW);
+        analogWrite(PB1B_MOTOR_ESQ, (int)(velocidade * ( porcentagem_motor_Esq_real )) ); 
+    }
+    else
+    {
+        digitalWrite(PB1A_MOTOR_ESQ, HIGH);
+        digitalWrite(PB1B_MOTOR_ESQ, HIGH);
+    }
+
+    if (direcao_dir == AVANCAR)
+    {
+        // 1 - % pq nesse sentido o calculo é invertido
+        int pwm = max(0, min(255, int(velocidade * (1.0 - porcentagem_motor_Esq_real))));
+        analogWrite(PA1A_MOTOR_DIR, pwm); 
+        digitalWrite(PA1B_MOTOR_DIR, LOW);
+    }
+    else if (direcao_dir == RETORNAR)
+    {
+        digitalWrite(PA1A_MOTOR_DIR, LOW);
+        analogWrite(PA1B_MOTOR_DIR, (int)(velocidade * ( porcentagem_motor_Dir_real ))); 
+    }
+    else
+    {
+        digitalWrite(PA1A_MOTOR_DIR, HIGH);
+        digitalWrite(PA1B_MOTOR_DIR, HIGH);
+    }
 }
